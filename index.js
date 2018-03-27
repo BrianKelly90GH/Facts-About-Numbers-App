05389b89a61f3cc8431d345c4696164889e30e28
Brian Kelly <briankelly90united@gmail.com>
2018-03-27 21:56:04 +0100

    added function to make call to the numbersapi and return results

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-alert(123);
+const selectedItem = document.getElementById('selectedItem');
+const numberInput = document.getElementById('numberInput');
+const factText = document.getElementById('factText')
+const factHeading= document.getElementById('factHeading');
+
+
+ numberInput.addEventListener('input', getFactFetch);
+
+ selectedItem.addEventListener('change', e => {
+   let number = numberInput.value;
+   if(number!=''){
+     getFactFetch();
+   }
+ });
+
+ // Fetch with Fetch API
+ function getFactFetch () {
+   let number = numberInput.value;
+   let category = selectedItem.value;
+   if(number != ''){
+     fetch('http://numbersapi.com/'+number+'/'+category).
+     then(response => response.text())
+     .then(data => {
+       factHeading.innerText = category+ ' fact about '+number;
+       factText.innerText = data;
+     })
+     .catch(err => console.log(err));
+   }
+   else{
+     factText.innerText = '';
+     factHeading.innerText ='';
+   }
+ }
